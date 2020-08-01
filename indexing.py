c072467596fb91d78e0aae0999d586f531a2b0ae
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-08-01 18:32:20 +0200

    support kmerSize

--- indexing.py
+++ indexing.py
@@ -6,21 +6,21 @@ import kProcessor as kp
 fasta_file = str()
 namesFile = str()
 
-if len(sys.argv) < 3:
-    sys.exit("run: python genes_indexing.py <fasta> <namesFiles>")
+if len(sys.argv) < 4:
+    sys.exit("run: python genes_indexing.py <fasta> <namesFiles> <kSize>")
 
 else:
     fasta_file = sys.argv[1]
     namesFile = sys.argv[2]
+    kSize = int(sys.argv[3])
 
 
 idx_suffix = os.path.basename(fasta_file).replace(".fa.names", "")
 print(f"Indexing {idx_suffix} ...", file=sys.stderr)
-kSize = 25
 kmers_mode = 1
 hashing_mode = 1
-chunk_size = 100
-kf_PHMAP = kp.kDataFramePHMAP(kSize, hashing_mode)
-#kf_PHMAP = kp.kDataFrameMQF(kSize, 28, hashing_mode)
+chunk_size = 10000
+#kf_PHMAP = kp.kDataFramePHMAP(kSize, hashing_mode)
+kf_PHMAP = kp.kDataFrameMQF(kSize, 29, hashing_mode)
 ckf = kp.index(kf_PHMAP, {"mode": kmers_mode}, fasta_file, chunk_size, namesFile)
 ckf.save(f"idx_{idx_suffix}")
