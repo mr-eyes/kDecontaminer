6f76bf1910433204542b92af316453458f52ba43
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-08-06 05:11:43 +0200

    genomes kmerCounting

---
+++ genomes_kmerCounting.py
@@ -0,0 +1,25 @@
+import kProcessor as kp
+from glob import glob
+import os
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("run: python genomes_kmerCounting.py <genomes_dir> <kDataframes_output_dir>")
+
+genomes_dir = sys.argv[1]
+output_directory = sys.argv[2]
+
+kSize = 21
+chunk_size = 10000
+hashing_mode = 1 # Integer hashing
+
+genomes = glob(f"{genomes_dir}/*")
+
+for genome_fasta in genomes:
+    print(f"counting {genome_fasta} kmers ...")
+    kf = kp.kDataFrameMQF(kSize)
+    kp.countKmersFromFile(kf,{"mode":hashing_mode},genome_fasta, chunk_size)
+    print(f"finished and counted {kf.size()} kmers...")
+    print(f"saving ...")
+    output_prefix = os.path.join(output_directory, "idx_" + os.path.basename(genomes))
+    kf.save(output_prefix)
