df8f7a0f1e7b8128439c471bd15ac4bded2fd720
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-09-08 23:17:24 +0200

    try

--- recover_missing.py
+++ recover_missing.py
@@ -227,10 +227,10 @@ tmp_job_pairs = list()
 
 for sample, genome in job_pairs:
     for finished_sample, finished_genome in finished:
-        print("-------------------------------------------------")
-        print(f"sample:{sample}, genome:{genome}")
-        print(f"finished_sample:{finished_sample}, finished_genome:{finished_genome}")
-        if sample in finished_sample and genome in finished_genome:
+        # print("-------------------------------------------------")
+        # print(f"sample:{sample}, genome:{genome}")
+        # print(f"finished_sample:{finished_sample}, finished_genome:{finished_genome}")
+        if finished_sample in sample and finished_genome in genome:
             break
     else:
         tmp_job_pairs.append((sample, genome))
