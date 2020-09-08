0a6fc134ba43806a99344ea3a6915143cac61f1e
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-09-08 23:13:34 +0200

    recovering missing

---
+++ recover_missing.py
@@ -0,0 +1,308 @@
+finished = [("Ast36C",	"GCA_000507305.1_ASM50730v1_genomic"),
+("AW3D",	"GCA_000507305.1_ASM50730v1_genomic"),
+("AW2C",	"GCA_000507305.1_ASM50730v1_genomic"),
+("Ast34D",	"GCA_000507305.1_ASM50730v1_genomic"),
+("Ast42B",	"GCA_000507305.1_ASM50730v1_genomic"),
+("AW8D",	"GCA_000507305.1_ASM50730v1_genomic"),
+("Ast25B",	"GCA_000507305.1_ASM50730v1_genomic"),
+("Ast35D",	"GCA_000507305.1_ASM50730v1_genomic"),
+("AW2C",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast42B",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast36C",	"GCA_001618945.1_ASM161894v1_genomic"),
+("AW8D",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast35D",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast34D",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast27B",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast25B",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast30A",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast29B",	"GCA_001618945.1_ASM161894v1_genomic"),
+("AW3D",	"GCA_001618945.1_ASM161894v1_genomic"),
+("Ast42B",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast36C",	"GCA_000211355.2_ASM21135v2_genomic"),
+("AW3D",	"GCA_000211355.2_ASM21135v2_genomic"),
+("AW2C",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast35D",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast34D",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast30A",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast25B",	"GCA_000211355.2_ASM21135v2_genomic"),
+("AW8D",	"GCA_000211355.2_ASM21135v2_genomic"),
+("Ast29B",	"GCA_000211355.2_ASM21135v2_genomic"),
+("AW3D",	"GCF_000149405.2_ASM14940v2_genomic"),
+("AW2C",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast42B",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast34D",	"GCF_000149405.2_ASM14940v2_genomic"),
+("AW8D",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast36C",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast35D",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast30A",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast27B",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast25B",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast45B",	"GCF_000149405.2_ASM14940v2_genomic"),
+("Ast29B",	"GCF_000149405.2_ASM14940v2_genomic"),
+("AW3D",	"con_Cfor.transcriptome.fasta"),
+("AW2C",	"con_Cfor.transcriptome.fasta"),
+("Ast42B",	"con_Cfor.transcriptome.fasta"),
+("AW8D",	"con_Cfor.transcriptome.fasta"),
+("Ast35D",	"con_Cfor.transcriptome.fasta"),
+("Ast36C",	"con_Cfor.transcriptome.fasta"),
+("Ast34D",	"con_Cfor.transcriptome.fasta"),
+("Ast27B",	"con_Cfor.transcriptome.fasta"),
+("Ast30A",	"con_Cfor.transcriptome.fasta"),
+("Ast25B",	"con_Cfor.transcriptome.fasta"),
+("AW3D",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("AW2C",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast34D",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast42B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast36C",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("AW8D",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast35D",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast25B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast30A",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast27B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast45B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast29B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast44B",	"GCA_000512085.1_Reti_assembly1.0_genomic"),
+("Ast42B",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast34D",	"GCF_000150955.2_ASM15095v2_genomic"),
+("AW3D",	"GCF_000150955.2_ASM15095v2_genomic"),
+("AW2C",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast36C",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast35D",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast30A",	"GCF_000150955.2_ASM15095v2_genomic"),
+("AW8D",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast25B",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast29B",	"GCF_000150955.2_ASM15095v2_genomic"),
+("Ast44B",	"GCF_000150955.2_ASM15095v2_genomic"),
+("AW3D",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("AW2C",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast42B",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast34D",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("AW8D",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast35D",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast36C",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast30A",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast27B",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast25B",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast29B",	"GCF_000372725.1_Emiliana_huxleyi_CCMP1516_main_genome_assembly_v1.0_genomic"),
+("Ast42B",	"GCA_003354225.1_ASM335422v1_genomic"),
+("AW3D",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast36C",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast34D",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast35D",	"GCA_003354225.1_ASM335422v1_genomic"),
+("AW8D",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast30A",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast25B",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast29B",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast44B",	"GCA_003354225.1_ASM335422v1_genomic"),
+("AW2C",	"GCA_003354225.1_ASM335422v1_genomic"),
+("Ast36C",	"GCF_000759875.1_ASM75987v1_genomic"),
+("AW3D",	"GCF_000759875.1_ASM75987v1_genomic"),
+("AW2C",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast34D",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast42B",	"GCF_000759875.1_ASM75987v1_genomic"),
+("AW8D",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast25B",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast35D",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast44B",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast27B",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast30A",	"GCF_000759875.1_ASM75987v1_genomic"),
+("Ast36C",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("AW3D",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("AW2C",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast42B",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast34D",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("AW8D",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast25B",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast35D",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast27B",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast44B",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("Ast30A",	"GCA_000397085.1_Porphyridium_purpureum_genomic"),
+("AW2C",	"con_Scar.genome_assembly.fa"),
+("AW3D",	"con_Scar.genome_assembly.fa"),
+("Ast42B",	"con_Scar.genome_assembly.fa"),
+("Ast34D",	"con_Scar.genome_assembly.fa"),
+("AW8D",	"con_Scar.genome_assembly.fa"),
+("Ast35D",	"con_Scar.genome_assembly.fa"),
+("Ast25B",	"con_Scar.genome_assembly.fa"),
+("Ast30A",	"con_Scar.genome_assembly.fa"),
+("Ast27B",	"con_Scar.genome_assembly.fa"),
+("Ast36C",	"con_Scar.genome_assembly.fa"),
+("Ast34D",	"con_Xtes.genome_assembly.fa"),
+("AW3D",	"con_Xtes.genome_assembly.fa"),
+("Ast36C",	"con_Xtes.genome_assembly.fa"),
+("AW2C",	"con_Xtes.genome_assembly.fa"),
+("Ast42B",	"con_Xtes.genome_assembly.fa"),
+("AW8D",	"con_Xtes.genome_assembly.fa"),
+("Ast25B",	"con_Xtes.genome_assembly.fa"),
+("Ast35D",	"con_Xtes.genome_assembly.fa"),
+("Ast30A",	"con_Xtes.genome_assembly.fa"),
+("Ast44B",	"con_Xtes.genome_assembly.fa"),
+("Ast27B",	"con_Xtes.genome_assembly.fa"),
+("Ast42B",	"coral_Mcav.genome_assembly.fasta"),
+("Ast35D",	"coral_Mcav.genome_assembly.fasta"),
+("AW3D",	"coral_Mcav.genome_assembly.fasta"),
+("Ast36C",	"coral_Mcav.genome_assembly.fasta"),
+("AW2C",	"coral_Mcav.genome_assembly.fasta"),
+("Ast34D",	"coral_Mcav.genome_assembly.fasta"),
+("Ast30A",	"coral_Mcav.genome_assembly.fasta"),
+("AW8D",	"coral_Mcav.genome_assembly.fasta"),
+("Ast25B",	"coral_Mcav.genome_assembly.fasta"),
+("Ast29B",	"coral_Mcav.genome_assembly.fasta"),
+("Ast34D",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast36C",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("AW2C",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast42B",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("AW8D",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast25B",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast35D",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast44B",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast30A",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast27B",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("AW3D",	"apoculata.assembly.scaffolds_chromosome_level.fasta"),
+("Ast36C",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("AW2C",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("AW3D",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast34D",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast42B",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast25B",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast35D",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("AW8D",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast44B",	"GCF_004143615.1_amil_sf_1.1_genomic"),
+("Ast34D",	"Cadsp1_AssemblyScaffolds.fasta"),
+("AW3D",	"Cadsp1_AssemblyScaffolds.fasta"),
+("AW2C",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast36C",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast35D",	"Cadsp1_AssemblyScaffolds.fasta"),
+("AW8D",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast30A",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast25B",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast29B",	"Cadsp1_AssemblyScaffolds.fasta"),
+("Ast42B",	"Cadsp1_AssemblyScaffolds.fasta")]
+
+import kProcessor as kp
+from glob import glob
+import os
+import sys
+import multiprocessing as MP
+import pickle
+import gc
+
+if len(sys.argv) < 5:
+    sys.exit("run: python genomes_kmerCounting.py <genomes_KFs_dir> <reads_cDBGs_dir> <output_file> <threads>")
+
+genomes_dir = sys.argv[1]
+reads_dir = sys.argv[2]
+output_file = sys.argv[3] + ".tsv"
+threads = int(sys.argv[4])
+
+kSize = 21
+chunk_size = 10000
+hashing_mode = 1  # Integer hashing
+
+samples_kfs = glob(reads_dir + "/*.mqf")
+genome_kfs = glob(genomes_dir + "/*.mqf")
+
+job_pairs = list()
+
+genomes_names = list()
+samples_names = list()
+sample_kmers = dict()
+
+for sample in samples_kfs:
+    sample = sample.replace(".mqf", '')
+    tmp_kf = kp.kDataFrame.load(sample)
+    sample_kmers[os.path.basename(sample)] = tmp_kf.size()
+    samples_names.append(os.path.basename(sample))
+
+    for genome in genome_kfs:
+        genome = genome.replace(".mqf", '')
+        genomes_names.append(os.path.basename(genome))
+        job_pairs.append((sample, genome))
+
+"""
+FILTER OUT FINISHED JOBS
+"""
+
+tmp_job_pairs = list()
+
+for sample, genome in job_pairs:
+    for finished_sample, finished_genome in finished:
+        if sample in finished_sample and genome in finished_genome:
+            break
+    else:
+        tmp_job_pairs.append((sample, genome))
+
+print(tmp_job_pairs)
+print(len(tmp_job_pairs))
+
+exit()
+
+manager = MP.Manager()
+
+intersection_count = manager.list()
+
+def get_intersection(pair):
+    global intersection_count
+    print(f"processing ({pair})")
+    sample_kf = kp.kDataFrame.load(pair[0])
+    genome_kf = kp.kDataFrame.load(pair[1])
+
+    intersection_kf = kp.kFrameIntersect([sample_kf, genome_kf])
+
+    common_kmers = intersection_kf.size()
+    sample_kmers = sample_kf.size()
+
+    sample_name = os.path.basename(pair[0])
+    genome_name = os.path.basename(pair[1])
+
+    intersection_count.append((sample_name, genome_name, common_kmers, sample_kmers))
+    print("---------------------------------")
+    print(pair)
+    print(sample_name, genome_name, common_kmers, sample_kmers)
+    print("---------------------------------")
+    del intersection_kf
+    print("Collecting garbage")
+    gc.collect()
+
+
+print(f"Processing started ...")
+
+try:
+    pool = MP.Pool(threads)
+    pool.map(get_intersection, job_pairs)
+finally:
+    pool.close()
+    pool.join()
+
+with open(f"{output_file}.pickle", "wb") as fp:
+    pickle.dump(intersection_count, fp)
+print("dumped the result in pickle ...")
+
+intersection_by_genome = dict()
+
+for _genome in genomes_names:
+    intersection_by_genome[_genome] = dict()
+
+for item in intersection_count:
+    sample_name, genome_name, common_kmers, unused_sample_kmers = item
+    intersection_by_genome[genome_name][sample_name] = common_kmers
+
+with open(output_file, 'w') as OUT:
+    header = str()
+    header += "ref.\t"
+    for sample_name, common_kmers in intersection_by_genome[genomes_names[0]].items():
+        sample = sample_name.replace(".mqf", '')
+        header += sample + '\t'
+    OUT.write(header[:-1] + '\n')
+
+    for genome_name, sample_dict in intersection_by_genome.items():
+        row = f"{genome_name}\t"
+        for sample_name, common_kmers in sample_dict.items():
+            containment = 100 * (common_kmers / sample_kmers[sample_name])
+            row += '%' + str(containment) + '\t'
+
+        OUT.write(row[:-1] + '\n')
+
+print("samples kmers:")
+for sample_name, kmers in sample_kmers.items():
+    print(f"\tsample({sample_name}): {kmers} kmers")
