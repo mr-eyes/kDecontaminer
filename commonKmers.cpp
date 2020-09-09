7b61c79ad3ee5ce7fb44f303c5698559242414ab
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-09-09 20:39:14 +0200

    log print

--- commonKmers.cpp
+++ commonKmers.cpp
@@ -33,14 +33,20 @@ uint64_t kf_size(kDataFrame * KF){
     return size;
 }
 
+std::string base_name(std::string const & path)
+{
+  return path.substr(path.find_last_of("/\\") + 1);
+}
+
 int main(int argc, char ** argv){
 
     string genome_prefix = argv[1];
     eraseSubStr(genome_prefix, ".mqf");
+    cerr << "Loading genome KF ... ";
     auto genomeKF = kDataFrame::load(genome_prefix);
+    cerr << "[DONE]" << endl;
 
     vector<string> samples_files;
-
     map<string, uint32_t> sample_to_commonKmers;
 
 
@@ -53,13 +59,17 @@ int main(int argc, char ** argv){
 
     for(auto const & sampleFile : samples_files){
         auto kf = kDataFrame::load(sampleFile);
+        cerr << "calculating common kmers ("<< sampleFile <<"&"<< genome_prefix <<") ... ";
         auto commonKmersKF = kProcessor::kFrameIntersect({kf, genomeKF});
+        cerr << "[DONE]" << endl;
         uint64_t intersection = kf_size(commonKmersKF);
         sample_to_commonKmers[sampleFile] = intersection;
     }
-   
+    
+    string genome_basename = base_name(genome_prefix);
+    cerr << "Wriring results to " << "contamStats_" + genome_basename + ".tsv" << " ..." << endl;
     ofstream fs;
-    string output_file_name = "contamStats_" + genome_prefix + ".tsv";
+    string output_file_name = "contamStats_" + genome_basename + ".tsv";
     fs.open(output_file_name);
     fs << "ref";
 
